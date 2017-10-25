044027d3d0b4a094f848c943e1b49083d21ab984
LeiHolmes <xulei964270141@qq.com>
2017-10-25 13:42:21 +0800

    scrollChange

--- app/src/main/java/com/leiholmes/rxbindingdemo/RxViewActivity.java
+++ app/src/main/java/com/leiholmes/rxbindingdemo/RxViewActivity.java
@@ -1,12 +1,16 @@
 package com.leiholmes.rxbindingdemo;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
 import com.jakewharton.rxbinding2.view.RxView;
+import com.jakewharton.rxbinding2.view.ViewScrollChangeEvent;
 
 import java.util.concurrent.TimeUnit;
 
@@ -28,8 +32,18 @@ public class RxViewActivity extends AppCompatActivity {
     Button btnLayout;
     @BindView(R.id.btn_change)
     Button btnChange;
+    @BindView(R.id.btn_draw)
+    Button btnDraw;
+    @BindView(R.id.view_canvas)
+    View viewCanvas;
+    @BindView(R.id.btn_scroll_layout)
+    Button btnScrollLayout;
+    @BindView(R.id.btn_scroll)
+    Button btnScroll;
     private CompositeDisposable mCompositeDisposable;
+    private int x;
 
+    @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,7 +51,9 @@ public class RxViewActivity extends AppCompatActivity {
         ButterKnife.bind(this);
         mCompositeDisposable = new CompositeDisposable();
         click();
+        draws();
         layoutChange();
+        scrollChange();
     }
 
     private void click() {
@@ -62,6 +78,28 @@ public class RxViewActivity extends AppCompatActivity {
                 }));
     }
 
+    private void draws() {
+        //点击btnDraw调用viewCanvas的绘制
+        addDisposable(RxView.clicks(btnDraw)
+                .throttleFirst(2, TimeUnit.SECONDS)
+                .subscribe(new Consumer<Object>() {
+                    @Override
+                    public void accept(Object o) throws Exception {
+                        //模拟让viewCanvas绘制,这个方法不好用
+//                        viewCanvas.getViewTreeObserver().dispatchOnDraw();
+                    }
+                }));
+        //当viewCanvas绘制时触发
+        addDisposable(RxView.draws(viewCanvas)
+                .subscribe(new Consumer<Object>() {
+                    @Override
+                    public void accept(Object o) throws Exception {
+                        Log.e("rx_binding_test", "draws:viewCanvas绘制了");
+                        Toast.makeText(RxViewActivity.this, "viewCanvas绘制了", Toast.LENGTH_SHORT).show();
+                    }
+                }));
+    }
+
     private void layoutChange() {
         //点击btnChange改变btn_layout的布局,防抖2s
         addDisposable(RxView.clicks(btnChange)
@@ -80,6 +118,32 @@ public class RxViewActivity extends AppCompatActivity {
                     public void accept(Object o) throws Exception {
                         Log.e("rx_binding_test", "layoutChanges:btnLayout布局改变了");
                         Toast.makeText(RxViewActivity.this, "btnLayout布局改变了", Toast.LENGTH_SHORT).show();
+                    }
+                }));
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    private void scrollChange() {
+        //点击btnScroll模拟让btnScrollLayout滑动
+        addDisposable(RxView.clicks(btnScroll)
+                .throttleFirst(2, TimeUnit.SECONDS)
+                .subscribe(new Consumer<Object>() {
+                    @Override
+                    public void accept(Object o) throws Exception {
+                        x += 10;
+                        if (x == 100) {
+                            x = 0;
+                        }
+                        btnScrollLayout.scrollTo(x, 0);
+                    }
+                }));
+        //btnScrollLayout滑动时触发
+        addDisposable(RxView.scrollChangeEvents(btnScrollLayout)
+                .subscribe(new Consumer<ViewScrollChangeEvent>() {
+                    @Override
+                    public void accept(ViewScrollChangeEvent viewScrollChangeEvent) throws Exception {
+                        Log.e("rx_binding_test", "scrollChangeEvents:btnScrollLayout滑动了:" + viewScrollChangeEvent.toString());
+                        Toast.makeText(RxViewActivity.this, "btnScrollLayout滑动了", Toast.LENGTH_SHORT).show();
                     }
                 }));
     }
