7ee21b5556420b9220e39b6f3055b75f858b2292
LeiHolmes <xulei964270141@qq.com>
2017-10-25 10:27:59 +0800

    layoutchanges

--- app/src/main/java/com/leiholmes/rxbindingdemo/MainActivity.java
+++ app/src/main/java/com/leiholmes/rxbindingdemo/MainActivity.java
@@ -1,81 +1,38 @@
 package com.leiholmes.rxbindingdemo;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
-import android.widget.Button;
-import android.widget.Toast;
+import android.view.View;
 
-import com.jakewharton.rxbinding.view.RxView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
-import java.util.concurrent.TimeUnit;
-
-import rx.Subscription;
-import rx.functions.Action1;
-import rx.subscriptions.CompositeSubscription;
-
-
+/**
+ * Description:   入口Activity
+ * author         xulei
+ * Date           2017/10/25 09:51
+ */
 public class MainActivity extends AppCompatActivity {
-    private Button btnShake;
-    private CompositeSubscription mCompositeSubscription;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mCompositeSubscription = new CompositeSubscription();
-        initView();
-        initBinding();
+        ButterKnife.bind(this);
     }
 
-    private void initView() {
-        btnShake = (Button) findViewById(R.id.btn_shake);
-    }
-
-    private void initBinding() {
-        //防抖点击
-        addSubscription(RxView.clicks(btnShake)
-                .throttleFirst(2, TimeUnit.SECONDS)
-                .subscribe(new Action1<Void>() {
-                    @Override
-                    public void call(Void aVoid) {
-                        Log.e("rx_binding_test", "点击了按钮：两秒内防抖");
-                        Toast.makeText(MainActivity.this, "点击了按钮：两秒内防抖", Toast.LENGTH_SHORT).show();
-                    }
-                }));
-        //长点击
-        addSubscription(RxView.longClicks(btnShake)
-                .subscribe(new Action1<Void>() {
-                    @Override
-                    public void call(Void aVoid) {
-                        Log.e("rx_binding_test", "长点击了按钮");
-                        Toast.makeText(MainActivity.this, "长点击了按钮", Toast.LENGTH_SHORT).show();
-                    }
-                }));
-    }
-
-    /**
-     * 添加订阅
-     */
-    public void addSubscription(Subscription subscription) {
-        if (mCompositeSubscription == null) {
-            mCompositeSubscription = new CompositeSubscription();
+    @OnClick({R.id.btn_rx_view, R.id.btn_rx_text_view, R.id.btn_rx_adapter_view, R.id.btn_rx_compound})
+    public void onViewClicked(View view) {
+        switch (view.getId()) {
+            case R.id.btn_rx_view:
+                startActivity(new Intent(this, RxViewActivity.class));
+                break;
+            case R.id.btn_rx_text_view:
+                break;
+            case R.id.btn_rx_adapter_view:
+                break;
+            case R.id.btn_rx_compound:
+                break;
         }
-        mCompositeSubscription.add(subscription);
-    }
-
-    /**
-     * 取消所有订阅
-     */
-    public void unSubscribe() {
-        if (mCompositeSubscription != null) {
-            mCompositeSubscription.clear();
-        }
-    }
-
-    @Override
-    protected void onDestroy() {
-        unSubscribe();
-        super.onDestroy();
     }
 }
--- app/src/main/java/com/leiholmes/rxbindingdemo/RxViewActivity.java
+++ app/src/main/java/com/leiholmes/rxbindingdemo/RxViewActivity.java
@@ -1,13 +1,111 @@
 package com.leiholmes.rxbindingdemo;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class RxViewActivity extends Activity {
+import com.jakewharton.rxbinding2.view.RxView;
+
+import java.util.concurrent.TimeUnit;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import io.reactivex.disposables.CompositeDisposable;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.functions.Consumer;
+
+/**
+ * Description:   RxBinding2中RxView演示Activity
+ * author         xulei
+ * Date           2017/10/25 09:51
+ */
+public class RxViewActivity extends AppCompatActivity {
+    @BindView(R.id.btn_click)
+    Button btnClick;
+    @BindView(R.id.btn_layout)
+    Button btnLayout;
+    @BindView(R.id.btn_change)
+    Button btnChange;
+    private CompositeDisposable mCompositeDisposable;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rx_view);
+        ButterKnife.bind(this);
+        mCompositeDisposable = new CompositeDisposable();
+        click();
+        layoutChange();
+    }
+
+    private void click() {
+        //2s防抖点击
+        addDisposable(RxView.clicks(btnClick)
+                .throttleFirst(2, TimeUnit.SECONDS)
+                .subscribe(new Consumer<Object>() {
+                    @Override
+                    public void accept(Object o) throws Exception {
+                        Log.e("rx_binding_test", "clicks:点击了按钮：两秒内防抖");
+                        Toast.makeText(RxViewActivity.this, "点击了按钮：两秒内防抖", Toast.LENGTH_SHORT).show();
+                    }
+                }));
+        //长点击
+        addDisposable(RxView.longClicks(btnClick)
+                .subscribe(new Consumer<Object>() {
+                    @Override
+                    public void accept(Object o) throws Exception {
+                        Log.e("rx_binding_test", "longClicks:长点击了按钮");
+                        Toast.makeText(RxViewActivity.this, "长点击了按钮", Toast.LENGTH_SHORT).show();
+                    }
+                }));
+    }
+
+    private void layoutChange() {
+        //点击btnChange改变btn_layout的布局,防抖2s
+        addDisposable(RxView.clicks(btnChange)
+                .throttleFirst(2, TimeUnit.SECONDS)
+                .subscribe(new Consumer<Object>() {
+                    @Override
+                    public void accept(Object o) throws Exception {
+                        btnLayout.layout(btnLayout.getLeft() - 20, btnLayout.getTop(),
+                                btnLayout.getRight() - 20, btnLayout.getBottom());
+                    }
+                }));
+        //btn_layout布局改变时触发
+        addDisposable(RxView.layoutChanges(btnLayout)
+                .subscribe(new Consumer<Object>() {
+                    @Override
+                    public void accept(Object o) throws Exception {
+                        Log.e("rx_binding_test", "layoutChanges:btnLayout布局改变了");
+                        Toast.makeText(RxViewActivity.this, "btnLayout布局改变了", Toast.LENGTH_SHORT).show();
+                    }
+                }));
+    }
+
+    /**
+     * 添加订阅
+     */
+    public void addDisposable(Disposable mDisposable) {
+        if (mCompositeDisposable == null) {
+            mCompositeDisposable = new CompositeDisposable();
+        }
+        mCompositeDisposable.add(mDisposable);
+    }
+
+    /**
+     * 取消所有订阅
+     */
+    public void unSubscribe() {
+        if (mCompositeDisposable != null) {
+            mCompositeDisposable.clear();
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        unSubscribe();
+        super.onDestroy();
     }
 }
